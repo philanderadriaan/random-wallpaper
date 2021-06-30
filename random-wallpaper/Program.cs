1525f31f8a70d3802a8fca621436cc8c686ca8a1
philanderadriaan <philanderadriaan@users.noreply.github.com>
2021-06-30 17:24:41 +0800

    Initial commit.

---
+++ random-wallpaper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace random_wallpaper
+{
+    class Program
+    {
+        static string PropFileName = "config.properties";
+        static string SrcKey = "src";
+        static string DestKey = "dest";
+
+        static void Main(string[] args)
+        {
+            log("Hello World!");
+        }
+
+        static void log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
